bf6f49294bc7de3b6bcc56590ede242e1dd07365
Isabella1807 <megaoreomuffin@hotmail.com>
2023-06-07 14:51:36 +0200

    Added the car calculator

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-console.log("index virker");
+console.log("index JS virker");
 
-const indexInfoObject = []
+const calculatedContainer = document.getElementById("indexCalculatedInformationContainer");
+
+const kml = document.getElementById("indexKMLInput");
+const km = document.getElementById("indexKMDagInput");
+const week = document.getElementById("indexPrWeekInput");
+
+const addTextToInfoContainer = () => {
+    const weeklyKM = km.value * week.value;
+    const weeklyGas = kml.value * weeklyKM;
+    const yearlyKM = km.value * 365;
+    const yearlyGas = kml.value * 365;
+    calculatedContainer.innerHTML = `Du kører i dag ${weeklyKM} km om ugen på ${weeklyGas} liter benzin. Dette svarer til ${yearlyKM} km om året eller ${yearlyGas} liter benzin om året`;
+}
+
+kml.addEventListener('input', addTextToInfoContainer);
+km.addEventListener('input', addTextToInfoContainer);
+week.addEventListener('input', addTextToInfoContainer);
